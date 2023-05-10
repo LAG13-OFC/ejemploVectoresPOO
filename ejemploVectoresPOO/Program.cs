06d38f8a90d5026af758ddea93bed7c67da99ace
lag13 <luca17sas@gmail.com>
2023-05-10 12:16:56 -0300

    Agregar archivos de proyecto.

---
+++ ejemploVectoresPOO/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploVectoresPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector objCP = new Vector();
+            objCP.VectorPrincipal  = new Int32[] { 11, 22, 33, 44, 55 };
+            objCP.VectorSecundario = new Double[] { 23, 10, 3, 20, 4 };
+            Int32[] AS = new Int32[5];
+            Int32 x = 0, j = 0, Pos, Venta = 1, pp = 0;
+            char S = 'j';
+            Boolean existe = true, pase= false;
+            Int32 primerPas = 1;
+
+            do
+            {
+                if (pase == false) { 
+                Console.Write("Ingrese el codigo del producto a vender o '0' para cerrar sistema de ventas: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    pase = true;
+                }
+                if (x == 0) { break; }
+                Pos = objCP.BuscarVectorInt32(x);
+                if (Pos == -1)
+                {
+                    
+                    Console.Write("No se encontro! Ingrese otro codigo: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    
+                }
+
+                else
+                {
+                    while (objCP.VectorSecundario[Pos] > 0)
+                    {
+                        if (S != 's' && existe == false || primerPas == 1)
+                        {
+                            primerPas = 0;
+                            Console.Write("Stock disponible: " + objCP.VectorSecundario[Pos] + " \nSeleccione la cantidad que desee vender: ");
+                            Venta = Convert.ToInt32(Console.ReadLine());
+
+                            objCP.Validacion(Venta,ref x, ref Pos, ref pp);
+                        }
+                        if(pp==1){
+                            primerPas = 0;
+                            Console.Write("Stock disponible: " + objCP.VectorSecundario[Pos] + " \nSeleccione la cantidad que desee vender: ");
+                            Venta = Convert.ToInt32(Console.ReadLine());
+
+                            objCP.Validacion(Venta,ref x, ref Pos, ref pp);
+                        }
+
+                        if (Venta <= objCP.VectorSecundario[Pos])
+                        {
+                            objCP.VectorSecundario[Pos] = objCP.VectorSecundario[Pos] - Venta;
+                            Console.WriteLine("¡VENTA EXITOSA! Se actualizo el stock del articulo: " + objCP.VectorPrincipal[Pos] + "\nCantidad de articulos disponibles: " + objCP.VectorSecundario[Pos]);
+                            Console.Write("¿Desea realizar otra venta? ¿S/N? ");
+                            S = Convert.ToChar(Console.ReadLine());
+                            char.ToLower(S);
+                            while (S != 's' && S != 'n')
+                            {
+                                Console.Write("OPCION INCORRECTA! INGRESE S/N: ");
+                                S = Convert.ToChar(Console.ReadLine());
+                                char.ToLower(S);
+                            }
+                        }
+
+
+                       Console.Clear();
+
+                        if (S == 's')
+                        {
+                            if (objCP.VectorSecundario[Pos] == 0)
+                            {                   
+                                Console.Write("Ingrese el codigo de otro articulo para vender o '0' para cerrar las ventas: ");
+                                x = Convert.ToInt32(Console.ReadLine());
+                                Pos = objCP.BuscarVectorInt32(x);
+                                if (x == 0) { break; }
+                                existe = false;
+                            }
+                            else
+                            {
+                                if (x == 0) { break; }
+                                Console.Write("Stock disponible: " + objCP.VectorSecundario[Pos] + " \nSeleccione la cantidad que desee vender: ");
+                                Venta = Convert.ToInt32(Console.ReadLine());
+
+                                objCP.Validacion(Venta,ref x, ref Pos, ref pp);
+
+                                objCP.VectorSecundario[Pos] = objCP.VectorSecundario[Pos] - Venta;
+                            }
+
+                        }
+                        if (S == 'n')
+                        {
+                            Console.Write("Ingrese el codigo del producto a vender o '0' para cerrar sistema de ventas: ");
+                            x = Convert.ToInt32(Console.ReadLine());
+                            if (x == 0) { break; }
+                            Pos = objCP.BuscarVectorInt32(x);
+                            Console.Write("Stock disponible: " + objCP.VectorSecundario[Pos] + " \nSeleccione la cantidad que desee vender: ");
+                            Venta = Convert.ToInt32(Console.ReadLine());
+                            objCP.Validacion(Venta,ref x, ref Pos, ref pp);
+                            if (x == 0) { break; }
+                            objCP.VectorSecundario[Pos] = objCP.VectorSecundario[Pos] - Venta;
+
+                        }
+
+                    }//fin while stock > 0    
+                }//fin else
+
+            } while(x!=0);
+
+            Console.Write("\nTiene: " + objCP.Generar(AS) + " articulos con menos de 10 en stock\n");
+            for (j = 0; j < objCP.Generar(AS); j++)
+            {
+                Console.WriteLine(AS[j]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejemploVectoresPOO/Vector.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploVectoresPOO
+{
+    internal class Vector
+    {
+        private Int32[] vectorPrincipal;
+        private Double[] vectorSecundario;
+
+
+        public int[] VectorPrincipal { get => vectorPrincipal; set => vectorPrincipal = value; }
+        public double[] VectorSecundario { get => vectorSecundario; set => vectorSecundario = value; }
+
+
+        public Vector()
+        {
+        }
+
+        public Int32 BuscarVectorInt32(Int32 x)
+        {
+            int i = 0, k = 0;
+            while (k == 0 && i < vectorPrincipal.Length)
+            {
+                if (vectorPrincipal[i] == x)
+                {
+                    k = 1;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            if (k == 0)
+            {
+                i = -1;
+            }
+            return i;
+
+
+        }
+        public void Validacion(Int32 Venta, ref Int32 x, ref Int32 Pos, ref Int32 pp)
+        {
+            //VALIDACION DE INGRESO DE VENTA
+            // int[] CP = new int[Stock.Length];
+            pp = 0;
+            while (Venta == 0 || Venta > vectorSecundario[Pos] || vectorSecundario[Pos] == 0)
+            {
+                if (Venta == 0)
+                {
+                    Console.Write("NO SE SELECCIONARON VENTAS! Seleccione la cantidad de articulos a vender: ");
+                    Venta = Convert.ToInt32(Console.ReadLine());
+                }
+                if (Venta > vectorSecundario[Pos])
+                {
+                    Console.WriteLine("STOCK INSUFICIENTE! CONTACTE A SU PROVEDOR");
+                    Console.Write("Ingrese el codigo del producto a vender o '0' para cerrar sistema de ventas: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    if (x != 0) { Pos = BuscarVectorInt32(x); }
+                   
+
+                }
+                if (x == 0) { break; }
+                if (vectorSecundario[Pos] == 0)
+                {
+                    Console.Write("Sin stock! Ingrese el codigo de otro articulo para vender o '0' para cerrar las ventas: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    if (x == 0) { break; }
+                }
+
+
+                else
+                {
+                    Venta = -1;
+                    pp = 1;
+                }
+
+            }
+        }
+
+        public Int32 Generar(Int32[] v3)
+        {
+            Int32 i = 0, j = 0;
+
+            for (i = 0; i < vectorSecundario.Length; i++)
+            {
+                if (vectorSecundario[i] < 10)
+                {
+                    v3[j] = vectorPrincipal[i];
+                    j++;
+                }
+            }
+            return j;
+
+        }
+    }
+}
